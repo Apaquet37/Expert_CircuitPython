cd8b70dc73ebdcd248352f11d73574ef752bb685
Abigail B Paquette <apaquet37@ccs.local>
2019-12-09 09:26:38 -0500

    Add CircuitPython to processing CircuitPython

--- CircuitPythonProcessing.py
+++ CircuitPythonProcessing.py
@@ -9,6 +9,14 @@ dial = AnalogIn(board.A2)
 #dial.direction = Direction.INPUT
 #dial.pull = Pull.UP 
 
+def get_voltage(pin):
+    return(pin.value*180)/65536
+
+
 while True:
-    time.sleep(.25)
-    uart.write(bytes([dial.value]))
+    time.sleep(.1)
+    myDial = get_voltage(dial)
+    #newVal = map_range(myDial, 0, 65536, 0, 255)
+    #data = uart.read(32)
+    uart.write(bytes([int(myDial)]))
+    print(myDial)
