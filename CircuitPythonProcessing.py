2f222cbb2396feb1922863cf9951866f9302aba3
Abigail B Paquette <apaquet37@ccs.local>
2020-01-16 09:29:05 -0500

    Comments added

--- CircuitPythonProcessing.py
+++ CircuitPythonProcessing.py
@@ -10,14 +10,14 @@ dial = AnalogIn(board.A2)
 #dial.pull = Pull.UP 
 
 def get_voltage(pin):
-    return(pin.value*180)/65536
+    return(pin.value*180)/65536 #getting and scaling my data, 0-180
 
 
 while True:
-    time.sleep(.1)
+    time.sleep(.1) #delay
     myDial = get_voltage(dial)
     #newVal = map_range(myDial, 0, 65536, 0, 255)  ---I didn't have the library needed to map, and I decided it would be easier to
                                                       #scale the number using the math at the top
     #data = uart.read(32)  ---Took this line out because I never used data, and it was slowing down my code a  ton
-    uart.write(bytes([int(myDial)]))
-    print(myDial)
+    uart.write(bytes([int(myDial)])) #my potentiometer values are being sent as bytes
+    print(myDial) #Printing the data to serial monitor
